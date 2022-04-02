d919768c57459dfd931ff48bb047c446655592b0
Nertsal <Nertsal@users.noreply.github.com>
2022-04-02 20:27:43 +0000

    deploy: 98aafd5cebae36b47bdf27c47ec9cf347a379af8

--- ludumdare50.js
+++ ludumdare50.js
@@ -738,24 +738,24 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper310 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 112, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper330 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 117, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper728 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 275, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper734 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 276, __wbg_adapter_23);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper729 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 275, __wbg_adapter_26);
+    imports.wbg.__wbindgen_closure_wrapper735 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 276, __wbg_adapter_26);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper730 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 275, __wbg_adapter_29);
+    imports.wbg.__wbindgen_closure_wrapper736 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 276, __wbg_adapter_29);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper731 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 275, __wbg_adapter_32);
+    imports.wbg.__wbindgen_closure_wrapper737 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 276, __wbg_adapter_32);
         return addHeapObject(ret);
     };
 
