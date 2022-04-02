b315ef1c01ad981e9163dac92c21c347201f4333
Nertsal <sasha.kudasov04@gmail.com>
2022-04-02 11:19:38 +0300

    Setup geng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use geng::prelude::*;
+
+pub struct State {
+    geng: Geng,
+}
+
+impl State {
+    pub fn new(geng: &Geng) -> Self {
+        Self { geng: geng.clone() }
+    }
+}
+
+impl geng::State for State {
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        ugli::clear(framebuffer, Some(Color::BLACK), None);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    logger::init().unwrap();
+    geng::setup_panic_handler();
+
+    let geng = Geng::new("Delay the inevitable");
+    let state = State::new(&geng);
+
+    geng::run(&geng, state);
 }
