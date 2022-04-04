dd3338fad783d87a3dbfe56dd6ec1abf053b3769
Nertsal <Nertsal@users.noreply.github.com>
2022-04-04 23:18:10 +0000

    deploy: cdfe8fed069d13d4d120ff6d6cc46c7c5b0e78cd

--- ludumdare50.js
+++ ludumdare50.js
@@ -816,8 +816,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper335 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 94, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper316 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 53, __wbg_adapter_22);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper907 = function(arg0, arg1, arg2) {
